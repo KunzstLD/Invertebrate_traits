08b49cdd20402fecfde2eb01815ac1ca5f9d6c2b
Kunzst <sk193@web.de>
2019-10-22 11:14:07 +0200

    Moved everything related to dispersal traits

--- R/01_EU_preprocessing_freshecol.R
+++ R/01_EU_preprocessing_freshecol.R
@@ -1,8 +1,8 @@
 # ------------------------------------------------------------------------------
-#### Preprocessing of freshwaterecology DB and incorporation of Tachet data ####
+#### Preprocessing of freshwaterecology DB ####
 # ------------------------------------------------------------------------------
 
-# Prep freshecol DB ---------------------------------------------------
+# read in
 dat_EU <- fread(
   file.path(
     data_in,
@@ -21,12 +21,6 @@ dat_EU[, (col) := lapply(.SD, function(y) {
   ifelse(!is.na(y), 1, NA)
 }), .SDcols = names(dat_EU) %like% "volt"]
 
-# dissem traits contain -99 -> change to NA
-# col <- grep("dissem", names(dat_EU), value = TRUE)
-# dat_EU[, (col) := lapply(.SD, function(y) {
-#   ifelse(y == -99, NA, y)
-# }), .SDcols = names(dat_EU) %like% "dissem"]
-
 # get trait columns
 cols <- grep("order|family|genus|species|unique_id", names(dat_EU), 
              value = TRUE, invert = TRUE)
@@ -37,48 +31,6 @@ dat_EU[, (cols) := lapply(.SD, as.numeric),
 for (j in cols){
   data.table::set(dat_EU, which(is.na(dat_EU[[j]])),j,0)
 }
-
-# Dispersal Data from Sarremejane -----------------------------------------
-# disp_EU <-
-#   fread(
-#     file = file.path(data_in, "Europe", "Flying_propensity.csv"),
-#     na.strings = ""
-#   )
-# 
-# # fly propensity NA to medium
-# disp_EU[grepl("NA", Flying_propensity), Flying_propensity := "Medium"]
-# 
-# # turn propensity trait into wide format
-# disp_EU <- dcast(
-#   data = disp_EU[!is.na(Flying_propensity)],
-#   formula = Taxon  ~ Flying_propensity,
-#   fun.aggregate = length,
-#   value.var = "Flying_propensity"
-# )
-# 
-# # filter for "dae"
-# # ** -> ony family level identification possible, average of traits for all genera
-# # in the given family
-# disp_EU[grepl(".*dae", Taxon), `:=`(Family = sub("\\*\\*|[[:space:]]\\*\\*", "",
-#                                                  Taxon),
-#                                     Taxon = NA_character_)]
-# # spp. to Genus
-# disp_EU[grepl("spp\\.", Taxon), `:=`(Genus = sub("[[:space:]]spp\\.", "", Taxon),
-#                                      Taxon = NA_character_)]
-# 
-# # * -> trait information from phylogenetically closest genera
-# disp_EU[grepl("\\*", Taxon), Taxon := sub("\\*","",Taxon)]
-# 
-# # del group in Taxon
-# disp_EU[grepl("group", Taxon), Taxon := sub("[[:space:]]group","", Taxon)]
-# 
-# # rename
-# setnames(disp_EU, "Taxon", "Species")
-# 
-# # change col type to numeric for Medium, Strong, Weak
-# disp_EU[, c("Medium", "Strong", "Weak") := lapply(.SD, as.numeric),
-#         .SDcols = c("Medium", "Strong", "Weak")]
-
 
 # Check duplicates ---------------------------------------------------
 # Have accidentially some entries been copied that contain the same information?
