01142a2166c9b03ad0bb9f7b42adb63429d0e528
Kunzst <sk193@web.de>
2019-10-25 16:46:41 +0200

    Minor changes

--- R/04_NOA_aggregate_traits.R
+++ R/04_NOA_aggregate_traits.R
@@ -138,11 +138,12 @@ Trait_Noa_agg[Trait_Noa,
 Trait_Noa_agg[, N := NULL]
 
 # Taxa resolved on family level already present in aggregated dataset
-Trait_Noa_agg[family %in% Trait_Noa[is.na(species) & is.na(genus),]$family, ]
+# Trait_Noa_agg[family %in% Trait_Noa[is.na(species) & is.na(genus),]$family, ]
 
 # merge back information on order
 Trait_Noa_agg[Trait_Noa,
               `:=`(order = i.order),
               on = "family"]
 # save
-saveRDS(object = Trait_Noa_agg, file = file.path(data_out, "Trait_Noa_agg.rds"))
+saveRDS(object = Trait_Noa_agg, 
+        file = file.path(data_out, "Trait_Noa_agg.rds"))
