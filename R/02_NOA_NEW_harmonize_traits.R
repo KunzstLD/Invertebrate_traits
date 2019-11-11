47e48179dcf34017f58691c752270f328f197dbf
Kunzst <sk193@web.de>
2019-11-11 17:18:09 +0100

    Added normalization & merge with old NOA DB

--- R/02_NOA_NEW_harmonize_traits.R
+++ R/02_NOA_NEW_harmonize_traits.R
@@ -1,13 +1,14 @@
 # _________________________________________________________________________ 
 #### Harmonize NOA Traits from Laura T ####
+# Includes also merge with old NOA trait DB (based on Vieira et al. 2006)
 # _________________________________________________________________________
 
 # read in
 Trait_Noa_new <- readRDS(file = file.path(data_cleaned, "North_America", "Traits_US_pp_LauraT.rds"))
 
 # _________________________________________________________________________
-#### Feed mode ####
-# feed_shredder: shredder (chewers, miners, xylophagus)
+#### Feeding mode ####
+# feed_shredder: shredder (chewers, miners, xylophagus, decomposing plants)
 # feed_gatherer: collector gatherer (gatherers, detritivores)
 # feed_filter: collector filterer (active filterers, passive filterers, absorbers)
 # feed_herbivore: herbivore piercers & scraper (grazer)
@@ -117,6 +118,147 @@ setnames(
 )
 
 # _________________________________________________________________________
+#### Normalize Noa New trait data ####
+# Normalizing of the trait values to a range of [0 - 1] by
+# dividing for a given trait each value for a trait state by the sum of all 
+# trait states
+# _________________________________________________________________________
+
+# get trait names & create pattern for subset
+trait_names_pattern <-
+  names(Trait_Noa_new[, -c("unique_id",
+                           "family",
+                           "genus",
+                           "species",
+                           "order")]) %>%
+  sub("\\_.*|\\..*", "", .) %>%
+  unique() %>%
+  paste0("^", .)
+
+# loop for normalization (trait categories for each trait sum up to 1) 
+for(cols in trait_names_pattern) {
+  
+  # get row sum for a specific trait
+  Trait_Noa_new[, rowSum := apply(.SD, 1, sum),
+                .SDcols = names(Trait_Noa_new) %like% cols]
+  
+  # get column names for assignment
+  col_name <- names(Trait_Noa_new)[names(Trait_Noa_new) %like% cols]
+  
+  Trait_Noa_new[, (col_name) := lapply(.SD, function(y) {
+    round(y / rowSum, digits = 2)
+  }),
+  .SDcols = names(Trait_Noa_new) %like% cols]
+}
+
+# del unnecessary columns
+Trait_Noa_new[, c("rowSum") := NULL]
+
+# _________________________________________________________________________
+#### Complement trait data with old NOA trait data (Vieira et al. 2006) ####
+# _________________________________________________________________________
+
+# Load harmoized & normalized Trait NOA
+Trait_Noa <- readRDS(file.path(data_cleaned, "North_America", "Traits_US_pp_harmonized.rds"))
+
+# Overlap Old & New NOA DB
+Trait_Noa <- Trait_Noa[order %in% c(
+  "Coleoptera",
+  "Diptera",
+  "Ephemeroptera",
+  "Hemiptera",
+  "Lepidoptera",
+  "Megaloptera",
+  "Neuroptera",
+  "Odonata",
+  "Plecoptera",
+  "Trichoptera"
+)]
+
+# _________________________________________________________________________
+#### Oviposition ####
+# ovip_aqu
+# ovip_ter
+# ovip_ovo
+# _________________________________________________________________________
+
+# Merge oviposition data (only present in old NOA trait DB) on species level 
+Trait_Noa_new[Trait_Noa[!is.na(species),],
+              `:=`(ovip_ter = i.ovip_ter,
+                   ovip_aqu = i.ovip_aqu,
+                   ovip_ovo = i.ovip_ovo),
+              on = "species"] 
+
+# Select relevant columns
+Trait_Noa_new <- Trait_Noa_new[, .SD,
+                               .SDcols = names(Trait_Noa_new) %like% "feed|locom|size|resp|volt|ovip|unique_id|species|genus|family|order"]
+
+# select columns from old Noa Trait DB that are also in Trait_Noa_New
+cols <- names(Trait_Noa)[names(Trait_Noa) %in% names(Trait_Noa_new)]
+Trait_Noa <- Trait_Noa[, .SD, .SDcols = cols]
+
+# get trait columns
+name_vec <- grep("unique_id|order|family|genus|species",
+                 names(Trait_Noa),
+                 value = TRUE,
+                 invert = TRUE) %>%
+  sub("\\_.*", "", .) %>%
+  unique() %>%
+  paste0("^",.)
+
+# coalesce merge on species level to complement missing information with the old trait DB
+# (likely that there isn't much that can be complemented by the old NOA trait DB)
+# % NA val before 
+# na_before <- sum(is.na(Trait_Noa_new))/(sum(is.na(Trait_Noa_new))+ sum(!is.na(Trait_Noa_new)))
+final <- Trait_Noa_new
+for(i in name_vec){
+  subset_vec <- !(rowSums(is.na(final[, .SD, .SDcols = names(final) %like% i])) == 0)
+  
+  # subset to NA values -> complement these with information from NOA old (if possible)
+  step <- coalesce_join(x = final[subset_vec, ],
+                        y = Trait_Noa[!is.na(species), .SD,
+                                   .SDcols = names(Trait_Noa) %like% paste0(i, "|", "species")],
+                        by = "species",
+                        join = dplyr::left_join)
+  setDT(step)
+  
+  # merge back to whole dataset
+  final <- coalesce_join(x = final,
+                         y = step[!is.na(species), 
+                                  .SD, .SDcols = names(step) %like% paste0(i, "|", "species")],
+                         by = "species",
+                         join = dplyr::left_join) 
+  setDT(final)
+}
+# % of na_values after merge -> slight improve
+# na_after <- sum(is.na(final))/(sum(is.na(final))+ sum(!is.na(final)))
+Trait_Noa_new <- final
+
+# merge information from taxa level that are only in old Noa DB
+# species level
+Trait_Noa_new <-
+  rbind(Trait_Noa_new, Trait_Noa[!is.na(species),] %>%
+          .[!(species %in% Trait_Noa_new[!is.na(species),]$species)])
+  
+# genus level & family level added
+# NOTE: no taxa has complete trait information (see uncommented code)
+# Trait_Noa[is.na(species) & !is.na(genus),] %>%
+#   .[!genus %in% Trait_Noa_new[is.na(species) & !is.na(genus),]$genus, ] %>% 
+#   na.omit(., cols = names(.[, -c("species")]))
+# Trait_Noa[is.na(species) & is.na(genus) & !is.na(family), ] %>%
+#   .[!family %in% Trait_Noa_new[is.na(species) & is.na(genus) & !is.na(family), ]$family,] %>% 
+#   na.omit(., cols = names(.[, -c("species", "genus")]))
+# genus level
+Trait_Noa_new  <-
+  rbind(Trait_Noa_new,
+        Trait_Noa[is.na(species) & !is.na(genus),] %>%
+          .[!genus %in% Trait_Noa_new[is.na(species) & !is.na(genus),]$genus, ])
+# family level
+Trait_Noa_new <- rbind(Trait_Noa_new,
+                       Trait_Noa[is.na(species) & is.na(genus) & !is.na(family), ] %>%
+                         .[!family %in% Trait_Noa_new[is.na(species) & is.na(genus) & !is.na(family), ]$family,])
+
+# _________________________________________________________________________
 #### Pattern fo development ####
 # Holometabolous 
 # Hemimetabolous
@@ -157,30 +299,6 @@ Trait_Noa_new[, `:=`(
   dev_hemimetabol = ifelse(order %in% hemimetabola, 1, 0),
   dev_holometabol = ifelse(order %in% holometabola, 1, 0)
 )]
-
-
-# select important traits & columns
-pattern <- "feed|locom|resp|volt|size|dev|unique_id|species|genus|family|order"
-Trait_Noa_new <- Trait_Noa_new[, .SD, .SDcols = names(Trait_Noa_new) %like% pattern]
-setcolorder(
-  Trait_Noa_new,
-  neworder = c(
-    "unique_id",
-    "species",
-    "genus",
-    "family",
-    "order",
-    "locom_sessil",
-    "locom_burrow",
-    "locom_swim",
-    "locom_crawl",
-    "feed_filter",
-    "feed_gatherer",
-    "feed_parasite",
-    "feed_predator",
-    "feed_shredder"
-  )
-)
 
 # save
 saveRDS(object = Trait_Noa_new, 
