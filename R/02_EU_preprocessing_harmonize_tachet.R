f35a2388c09afc8cb7593ee89f32be095a18eb44
Kunzst <sk193@web.de>
2019-11-11 17:19:52 +0100

    Adapted new harmonization for resp & feeding mode

--- R/02_EU_preprocessing_harmonize_tachet.R
+++ R/02_EU_preprocessing_harmonize_tachet.R
@@ -173,15 +173,19 @@ tachet[, `:=`(ph_acidic = apply(.SD, 1, max)),
 # del other ph columns
 tachet[, c("Ph_4_t", "Ph_445_t", "Ph_455_t", "Ph_555_t", "Ph_556_t", "Ph_6_t") := NULL]
 
+# _________________________________________________________________________
 # Voltinism
 # volt_semi
 # volt_uni
 # volt_bi_multi
+# _________________________________________________________________________
 
+# _________________________________________________________________________
 # Size
 # size_small: size < 9 mm (EU: size < 10 mm)
 # size_medium: 9 mm < size > 16 mm (EU: 10 mm < size > 20 mm)
 # size_large: size > 16 mm (EU: size > 20 mm)
+# _________________________________________________________________________
 setnames(tachet, "Size_4", "size_medium")
 tachet[, size_small := apply(.SD, 1, max),
        .SDcols = c("Size_1", "Size_2", "Size_3")]
@@ -193,11 +197,13 @@ tachet[, c(
   "Size_6", "Size_7"
 ) := NULL]
 
+# _________________________________________________________________________
 # Locomotion
 # locm_swim:  swimmer, scater (active & passive)
 # locm_crawl: crawlers, walkers & sprawler
 # locm_burrow: burrower
 # locm_sessil: sessil (attached)
+# _________________________________________________________________________
 tachet[, locom_swim := apply(.SD, 1, max), 
        .SDcols = c("locom_swim_full", "locom_swim_dive")]
 tachet[, locom_sessil := apply(.SD, 1, max), 
@@ -206,34 +212,43 @@ tachet[, c("locom_swim_full", "locom_swim_dive",
            "locom_sessil_temp", 
            "loc_flier_t", "loc_interstitial_t") := NULL]
 
+# _________________________________________________________________________
 # Respiration
 # resp_teg: cutaneous/tegument
 # resp_gil: gills
-# resp_spi: spiracle
-# resp_pls: plastron
-# respiration vesicle deleted -> only two entries
+# resp_pls_spi: plastron & spiracle
+  # plastron & spiracle often work together in respiratory systems of aq. insects
+  # Present in insects with open tracheal systems -> breathe oxygen from the air
+  # -> Different tolerances to low oxygen compared to insects with tegument resp and gills
+# TODO: currently respiration vesicle is deleted -> only two entries
+# _________________________________________________________________________
+tachet[, resp_pls_spi := apply(.SD, 1, max),
+        .SDcols = c("resp_pls", "resp_spi")]
 tachet[, resp_ves := NULL]
 
+# _________________________________________________________________________
 # Feeding mode
-# feed_shredder: shredder (chewers, miners, xylophagus, herbivore piercers)
+# feed_shredder: shredder (chewers, miners, xylophagus, decomposing plants)
 # feed_gatherer: collector gatherer (gatherers, detritivores)
 # feed_filter: collector filterer (active filterers, passive filterers, absorbers)
-# feed_scraper: scraper (grazer)
+# feed_herbivore: herbivore piercers & scraper (grazer)
 # feed_predator: predator
 # feed_parasite: parasite
+# _________________________________________________________________________
 tachet[, feed_filter := apply(.SD, 1, max),
        .SDcols = c("feed_active_filter", "feed_active_filter_abs")]
-tachet[, feed_shredder := apply(.SD, 1, max), 
-       .SDcols = c("feed_shredder", "feed_piercer_t")]
+tachet[, feed_herbivore := apply(.SD, 1, max),
+       .SDcols = c("feed_scraper", "feed_piercer_t")]
 # del columns
 tachet[, c("feed_active_filter",
            "feed_active_filter_abs",
            "feed_piercer_t") := NULL]
-
+# _________________________________________________________________________
 # Oviposition
 # ovip_aqu: Reproduction via aquatic eggs
 # ovip_ter: Reproduction via terrestric eggs
 # ovip_ovo: Reproduction via ovoviparity
+# _________________________________________________________________________
 setnames(tachet,
          c("rep_clutch_terr", "rep_ovovipar"),
          c("ovip_ter", "ovip_ovo"))
@@ -252,9 +267,10 @@ tachet[, c("rep_egg_free_iso",
            "rep_clutch_free",
            "rep_clutch_veg", 
            "rep_asexual") := NULL]
-
+# _________________________________________________________________________
 #### Pattern of development ####
 # Holometabolous or hemimetabolous
+# _________________________________________________________________________
 hemimetabola <- c(
   "Ephemeroptera",
   "Odonata",
