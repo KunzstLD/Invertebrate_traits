30cdf4a681b39ce6ad1bb3794c02fb1307505bd3
Kunzst <sk193@web.de>
2019-11-24 17:15:55 +0100

    Uncommented fun

--- R/Unit_tests/testing_normalize_by_rowSum.R
+++ R/Unit_tests/testing_normalize_by_rowSum.R
@@ -3,16 +3,16 @@
 # tests the creation of pattern from column names
 #______________________________________________________________
 
-fun <- function(x, non_trait_cols) {
-  
-  # get trait names & create pattern for subset
-  pat <- paste0(non_trait_cols, collapse = "|")
-  trait_names_pattern <-
-    grep(pat, names(x), value = TRUE, invert = TRUE) %>%
-    sub("\\_.*|\\..*", "", .) %>%
-    unique() %>%
-    paste0("^", .)
-}
+# fun <- function(x, non_trait_cols) {
+#   
+#   # get trait names & create pattern for subset
+#   pat <- paste0(non_trait_cols, collapse = "|")
+#   trait_names_pattern <-
+#     grep(pat, names(x), value = TRUE, invert = TRUE) %>%
+#     sub("\\_.*|\\..*", "", .) %>%
+#     unique() %>%
+#     paste0("^", .)
+# }
 
 # expected values
 value_to_expect <- c(
