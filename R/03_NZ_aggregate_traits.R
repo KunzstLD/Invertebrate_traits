f67dd39e3ee54885b703d8185ee13b84a4e33983
Kunzst <sk193@web.de>
2019-12-02 15:39:07 +0100

    Simplified completeness of trait data

--- R/03_NZ_aggregate_traits.R
+++ R/03_NZ_aggregate_traits.R
@@ -18,32 +18,12 @@ Trait_NZ <- normalize_by_rowSum(
 )
 
 # test how complete trait sets are 
-trait_names_pattern <-
-  names(Trait_NZ[, -c("family",
-                      "genus",
-                      "species",
-                      "order"
-  )]) %>%
-  sub("\\_.*|\\..*", "", .) %>%
-  unique() %>%
-  paste0("^", .)
-
-output <- matrix(ncol = 2, nrow = length(trait_names_pattern))
-
-for (i in seq_along(trait_names_pattern)) {
-  # vector containing either 0 (no NAs) or a number (> 0) meaning that all
-  # entries for this trait contained NA
-  vec <-
-    Trait_NZ[, apply(.SD, 1, function(y)
-      base::sum(is.na(y))),
-      .SDcols = names(Trait_NZ) %like% trait_names_pattern[[i]]]
-  
-  # How complete is the dataset for each individual trait?
-  output[i,] <-
-    c((length(vec[vec == 0]) / nrow(Trait_NZ))  %>% `*` (100) %>% round(),
-      trait_names_pattern[[i]])
-}
-# output
+# 90 % dev -> the other 10 % are not aquatic insects
+completeness_trait_data(x = Trait_NZ,
+                        non_trait_cols = c("order",
+                                           "family",
+                                           "genus",
+                                           "species"))
 
 # just return rows where for each trait there is an observation 
 Trait_NZ <- na.omit(Trait_NZ,
@@ -77,16 +57,18 @@ trait_col <- names(Trait_NZ[, -c("family",
                                  "species",
                                  "order")])
 
-# subset so that no NA values occur in Species data (otherwise all NA entries are viewed as a group &
-# aggregated as well)
+# subset so that no NA values occur in Species data 
+# (otherwise all NA entries are viewed as a group & aggregated as well)
 Trait_NZ_genus <- Trait_NZ[!is.na(species), lapply(.SD, median), 
                            .SDcols = trait_col, 
                            by = genus]
+
 # merge family information back
 Trait_NZ_genus[Trait_NZ[!is.na(species), ], 
                `:=`(family = i.family,
                     order = order),
                on = "genus"]
+
 # bind with data resolved on Genus level 
 Trait_NZ_genus <-
   rbind(Trait_NZ_genus, Trait_NZ[is.na(species) &
@@ -129,20 +111,16 @@ Trait_fam[Trait_NZ_genus,
 
 # filter for taxa resolved on family level that are not present in aggregated dataset
 # Those will be added as well
-Trait_NZ_resol_fam <-
+Taxa_famlvl <-
   Trait_NZ[is.na(species) & is.na(genus) & !is.na(family), ] %>%
   .[!family %in% Trait_fam$family, -c("species", "genus")]
-
 # no duplicates
-# Trait_NZ_resol_fam$family %>% duplicated()
+# Taxa_famlvl$family %>% duplicated()
 
 # rbind with trait data resolved on family level
-Trait_NZ_agg <- rbind(Trait_NZ_resol_fam, Trait_fam) 
+Trait_NZ_agg <- rbind(Taxa_famlvl, Trait_fam) 
 
-# rm dev_no_insect category (just zeros)
-Trait_NZ_agg[, dev_no_insect := NULL]
-
-# feeding mode parasite not present in NZ -> add artificial column
+# feeding mode parasite not present in NZ -> add "artificial" column
 Trait_NZ_agg[, feed_parasite := rep(0, nrow(Trait_NZ_agg))]
 
 # save
