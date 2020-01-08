e949a28148cc18e34f5c04bcbab30c26d110ef60
Kunzst <sk193@web.de>
2020-01-08 21:45:06 +0100

    Completed calc ratio agg/init fam per dataset

--- R/Ratio_fam_agg_init.R
+++ R/Ratio_fam_agg_init.R
@@ -3,7 +3,7 @@
 # (not aggregated) trait datasets?
 # This gives us insight how much and how consistent
 # information is covered in these trait databases
-
+# TODO: Identify missing families
 
 # _______________________________________________
 #### Load initial & aggregated final dataset ####
@@ -28,13 +28,9 @@ for (i in seq_along(file_names)) {
 }
 
 EU_init <- datasets_init[c(".Trait_EU_pp_harmonized", ".Trait_Tachet_pp_harmonized")]
-NOA_init <- datasets_init[c(".Trait_US_LauraT_pp_harmonized", "Trait_US_pp_harmonized")]
-
-# TODO: Check Zeallot
-# c(AUS_init, NZ_init) %<-% c(datasets_init$.Trait_AUS_harmonized,
-#                             datasets_init$.Trait_NZ_pp_harmonized)
-
-str(datasets_init$.Trait_AUS_harmonized)
+NOA_init <- datasets_init[c(".Traits_US_LauraT_pp_harmonized", ".Traits_US_pp_harmonized")]
+c(AUS_init, NZ_init) %<-% list(datasets_init$.Trait_AUS_harmonized,
+                            datasets_init$.Trait_NZ_pp_harmonized)
 
 # aggregated datasets
 files_agg <- list.files(path = file.path(data_out), pattern = ".*\\.rds")
@@ -67,7 +63,12 @@ idcol = "file"
 ) %>%
   .[, .(family, .N), by = .(file, order)]
 
-# EU
+#### EU ####
+# prepare aggregated data for EU
+cov_agg_EU <- cov_agg[grepl("Trait_EU.*", file), ] %>%
+  .[!duplicated(order), -("family")]
+
+# EU initial data
 cov_EU_init <- rbindlist(lapply(EU_init, function(y)
   y[order %in% c(
     "Ephemeroptera",
@@ -82,20 +83,122 @@ cov_EU_init <- rbindlist(lapply(EU_init, function(y)
   .[!duplicated(family), .(file, family, .N),
     by = order]
 
-cov_agg_EU <- cov_agg[grepl("Trait_EU.*", file), ] %>%
-  .[!duplicated(order), -("family")]
-
+# Calculate taxonomical coverage EU
 cov_agg_EU[cov_EU_init,
   `:=`(N_init = i.N),
   on = "order"
 ] %>%
-  .[, .(file,
-        order,
-        Number_agg_families = N,
-        Number_initial_families = N_init,
-        taxonomical_coverage = N / N_init
+  .[, `:=`(
+    file = file,
+    order = order,
+    Number_agg_families = N,
+    Number_initial_families = N_init,
+    taxonomical_coverage = N / N_init
   )]
 
-# NoA
+#### NOA ####
+# prepare aggregated data NOA
+cov_agg_NOA <- cov_agg[grepl("Trait_Noa.*", file), ] %>%
+  .[!duplicated(order), -("family")]
 
-#AUS & NZ
+# NOA initial data
+cov_init_NOA <- rbindlist(lapply(NOA_init, function(y)
+  y[order %in% c(
+    "Ephemeroptera",
+    "Hemiptera",
+    "Odonata",
+    "Trichoptera",
+    "Coleoptera",
+    "Plecoptera",
+    "Diptera"
+  ), .(family, order)]),
+  idcol = "file") %>%
+  .[!duplicated(family), .(file, family, .N),
+    by = order]
+
+# Calculate taxonomical coverage NOA
+cov_agg_NOA[cov_init_NOA,
+  `:=`(N_init = i.N),
+  on = "order"
+] %>%
+  .[, `:=`(
+    file = file,
+    order = order,
+    Number_agg_families = N,
+    Number_initial_families = N_init,
+    taxonomical_coverage = N / N_init
+  )]
+
+#### AUS ####
+# prepare aggregated data NOA
+cov_agg_AUS <- cov_agg[grepl("Trait_AUS.*", file), ] %>%
+  .[!duplicated(order), -("family")]
+
+# NOA initial data
+cov_init_AUS <- AUS_init[order %in% c(
+  "Ephemeroptera",
+  "Hemiptera",
+  "Odonata",
+  "Trichoptera",
+  "Coleoptera",
+  "Plecoptera",
+  "Diptera"
+), .(family, order)] %>%
+  .[!duplicated(family), .(family, .N),
+    by = order
+  ]
+
+# Calculate taxonomical coverage NOA
+cov_agg_AUS[cov_init_AUS,
+  `:=`(N_init = i.N),
+  on = "order"
+] %>%
+  .[, `:=`(
+    file = file,
+    order = order,
+    Number_agg_families = N,
+    Number_initial_families = N_init,
+    taxonomical_coverage = N / N_init
+  )]
+
+ #### NZ ####
+cov_agg_NZ <- cov_agg[grepl("Trait_NZ.*", file), ] %>%
+  .[!duplicated(order), -("family")]
+
+# NOA initial data
+cov_init_NZ <- NZ_init[order %in% c(
+  "Ephemeroptera",
+  "Hemiptera",
+  "Odonata",
+  "Trichoptera",
+  "Coleoptera",
+  "Plecoptera",
+  "Diptera"
+), .(family, order)] %>%
+  .[!duplicated(family), .(family, .N),
+    by = order
+  ]
+
+# Calculate taxonomical coverage NOA
+cov_agg_NZ[cov_init_NZ,
+  `:=`(N_init = i.N),
+  on = "order"
+] %>%
+  .[, `:=`(
+    file = file,
+    order = order,
+    Number_agg_families = N,
+    Number_initial_families = N_init,
+    taxonomical_coverage = N / N_init
+  )]
+
+# bring all datasets together
+tax_coverage <- rbind(
+  cov_agg_EU, cov_agg_NOA,
+  cov_agg_AUS, cov_agg_NZ
+)
+
+# convert to wide table
+data.table::dcast(tax_coverage[, .(file, order, taxonomical_coverage)],
+                  formula = file ~ order,
+                  value.var = "taxonomical_coverage")
