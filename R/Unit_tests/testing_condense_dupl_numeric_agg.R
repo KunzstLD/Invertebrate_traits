c2d00db473efbd26a348fe9247bbf7d2cba0bf1a
Kunzst <sk193@web.de>
2020-02-01 18:28:49 +0100

    Added (0,1) case

--- R/Unit_tests/testing_condense_dupl_numeric_agg.R
+++ R/Unit_tests/testing_condense_dupl_numeric_agg.R
@@ -2,24 +2,21 @@
 #### testing condense_dupl_numeric_agg ####
 #____________________________________________________
 
-test_that(
-  "testing output of condense_dupl_numeric_agg",
-  {
-  expect_equal(condense_dupl_numeric_agg(NA), NA)  
-  expect_equal(condense_dupl_numeric_agg(0), 0)  
-  expect_equal(condense_dupl_numeric_agg(1), 1)
-  expect_equal(condense_dupl_numeric_agg(c(1,1)), 1)
-  expect_equal(condense_dupl_numeric_agg(c(2,1)), 1.5)
-  expect_equal(condense_dupl_numeric_agg(c(1,1,0)), 1)
-  expect_equal(condense_dupl_numeric_agg(c(1,0,1)), 1)
-  expect_equal(condense_dupl_numeric_agg(c(0,1,1)), 1)
-  expect_equal(condense_dupl_numeric_agg(c(0,0,0)), 0)
-  expect_equal(condense_dupl_numeric_agg(c(1,0,0)), 1)
-  expect_equal(condense_dupl_numeric_agg(c(0,1,0)), 1)
-  expect_equal(condense_dupl_numeric_agg(c(0,0,1)), 1)
-  expect_equal(condense_dupl_numeric_agg(c(3,2,1)), 2)
-  expect_equal(condense_dupl_numeric_agg(c(3,2,0)), 2.5)
-  }  
-)
-
-
+test_that("testing output of condense_dupl_numeric_agg",
+          {
+            expect_equal(condense_dupl_numeric_agg(NA), NA)
+            expect_equal(condense_dupl_numeric_agg(0), 0)
+            expect_equal(condense_dupl_numeric_agg(1), 1)
+            expect_equal(condense_dupl_numeric_agg(c(0, 1)), 1)
+            expect_equal(condense_dupl_numeric_agg(c(1, 1)), 1)
+            expect_equal(condense_dupl_numeric_agg(c(2, 1)), 1.5)
+            expect_equal(condense_dupl_numeric_agg(c(1, 1, 0)), 1)
+            expect_equal(condense_dupl_numeric_agg(c(1, 0, 1)), 1)
+            expect_equal(condense_dupl_numeric_agg(c(0, 1, 1)), 1)
+            expect_equal(condense_dupl_numeric_agg(c(0, 0, 0)), 0)
+            expect_equal(condense_dupl_numeric_agg(c(1, 0, 0)), 1)
+            expect_equal(condense_dupl_numeric_agg(c(0, 1, 0)), 1)
+            expect_equal(condense_dupl_numeric_agg(c(0, 0, 1)), 1)
+            expect_equal(condense_dupl_numeric_agg(c(3, 2, 1)), 2)
+            expect_equal(condense_dupl_numeric_agg(c(3, 2, 0)), 2.5)
+          })
