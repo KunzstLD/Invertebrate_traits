263649c1bed4453c02450b76f1e55b1aa3f76b51
Kunzst <sk193@web.de>
2020-02-01 18:24:20 +0100

    minor changes

--- R/01_NOA_preprocessing_taxa.R
+++ R/01_NOA_preprocessing_taxa.R
@@ -21,14 +21,14 @@ Trait_Noa <- Trait_Noa[rowSums(is.na(Trait_Noa)) != ncol(Trait_Noa), ]
 # all dae entries in Taxon are the same in col family
 # Trait_Noa[grepl(".*dae", Taxon), .(which(Family != Taxon))]
 # Trait_Noa[grepl(".*dae", Taxon)][c(35, 36, 37, 189), .(Family, Taxon)]
-# all spp. taxa in Taxon column have an entry in Genus 
+# all spp. taxa in Taxon column have an entry in Genus
 # all(Trait_Noa[Taxon %like% "spp.", .(Genus = !is.na(Genus))]$Genus)
 Trait_Noa[, Species := ifelse(Taxon %like% "spp.|sp.", NA, Taxon)]
 Trait_Noa[, Species := ifelse(Species %like% ".+\\s.+", Species, NA)]
 
 # few families/order in Taxon left
 Trait_Noa[is.na(Species) & is.na(Genus) & is.na(Family) &
-            grepl(".+nae", Taxon), Family := Taxon]
+  grepl(".+nae", Taxon), Family := Taxon]
 Trait_Noa[is.na(Species) & is.na(Genus) & is.na(Family), Order := Taxon]
 
 # Change col order
@@ -44,12 +44,15 @@ saveRDS(Trait_Noa, file = file.path(data_in, "North_America", "Inverttraitstable
 # retrieve information on order
 family_Noa <- get_ids(names = Trait_Noa$Family, db = "gbif", rows = 1)
 Trait_Noa[, id_family := family_Noa$gbif]
-order_Noa <- cbind(classification(id = Trait_Noa[!is.na(id_family) & !duplicated(id_family), ]$id_family,
-                                  db = "gbifid"))[c("family", "order")]
+order_Noa <- cbind(classification(
+  id = Trait_Noa[!is.na(id_family) & !duplicated(id_family), ]$id_family,
+  db = "gbifid"
+))[c("family", "order")]
 setDT(order_Noa)
 Trait_Noa[order_Noa,
-          `:=`(Order = i.order),
-          on = c(Family = "family")]
+  `:=`(Order = i.order),
+  on = c(Family = "family")
+]
 
 # check order for (sub) families taxizie couldn't recognize
 Trait_Noa[is.na(id_family) & grepl("Dicosmoecinae", Family), Order := "Trichoptera"]
@@ -67,8 +70,11 @@ Trait_Noa[grepl("Caenestheriidae", Family), Order := "Branchiopoda"]
 Trait_Noa[, id_family := NULL]
 
 # save
-saveRDS(object = Trait_Noa, 
-        file = file.path(data_cleaned, 
-                         "North America", 
-                         "Traits_US_taxa_pp.rds")
+saveRDS(
+  object = Trait_Noa,
+  file = file.path(
+    data_cleaned,
+    "North America",
+    "Traits_US_taxa_pp.rds"
+  )
 )
