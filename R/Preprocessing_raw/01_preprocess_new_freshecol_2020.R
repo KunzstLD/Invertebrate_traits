0f923f571545d8501e35b8c795832b9eaf8a90e3
Kunzst <sk193@web.de>
2020-09-14 19:41:06 +0200

    14.09.2020: taxonomic proc new freshecol

--- R/Preprocessing_raw/01_preprocess_new_freshecol_2020.R
+++ R/Preprocessing_raw/01_preprocess_new_freshecol_2020.R
@@ -1,8 +1,15 @@
+
+# link to raw data
 data_raw <- "./Data/EU/Raw_freshecol/2020"
 
-
+# packages
 library(data.table)
 library(dplyr)
+
+# script with helper functions
+source(file = "./R/Preprocessing_raw/functions_used.R")
+
+
 
 #### read taxaexports ####
 filelinks <- list.files(
@@ -40,10 +47,12 @@ output_db <- lapply(
 # rm col V1 in all but one data table
 lapply(
   output_db[2:length(output_db)],
-  function(y) y[, V1 := NULL]
+  function(y) {
+    y[, c("V1", "EU") := NULL]
+  }
 )
 
-# cbind
+# bind
 freshwaterecol <- Reduce(f = cbind, output_db)
 setnames(freshwaterecol, "V1", "taxon")
 
@@ -51,28 +60,55 @@ setnames(freshwaterecol, "V1", "taxon")
 # copy of taxon column
 freshwaterecol[, taxon_cp := taxon]
 
-# - move DAE and similar into family column
+# - rm Ad. from taxon column
+freshwaterecol <- freshwaterecol[!grep("Ad\\.", taxon), ]
+
+# - move just CAPTIAL family taxa to new column
+# & fill empty spots
 freshwaterecol[
-  grep(".*NAE|.*DAE|.*dae|.*nae", taxon),
+  grep(".*NAE|.*DAE", taxon),
   `:=`(
     family = taxon,
     taxon = NA_character_
   )
 ]
+freshwaterecol[, family := copy_till_next_occur(x = family)]
 
-# - Class/Phlyum/Order entries into order_or_higher column
-freshwaterecol[grep(
-  "^[A-z]{1,}$|\\[",
-  taxon
-), `:=`(
-  order_or_higher = taxon,
-  taxon = NA_character_
-)]
+# - Class/Phylum/Order entries into order_or_higher column
+# & fill empty spots
+freshwaterecol[
+  grep("^[A-z]{1,}$|\\[", taxon),
+  `:=`(
+    order_or_higher = taxon,
+    taxon = NA_character_
+  )
+]
+freshwaterecol[, order_or_higher := copy_till_next_occur(x = order_or_higher)]
 
-# - rm Ad. from taxon column
-freshwaterecol <- freshwaterecol[!grep("Ad\\.", taxon), ]
+# Delete gen. sp
+# TODO: How to handle later in analysis?
+freshwaterecol[grep("Gen\\. sp\\.", taxon), taxon := NA_character_]
 
-# - everything with sp. to genus column
+# - sort out all taxa with zero information
+trait_cols <- grep("taxon.*|EU|genus|family|order.*",
+  names(freshwaterecol),
+  value = TRUE,
+  invert = TRUE
+)
+
+no_info <- freshwaterecol[, apply(.SD, 1, function(y) {
+  is.na(y) %>%
+    sum()
+}),
+.SDcols = trait_cols
+]
+pos <- no_info == ncol(freshwaterecol[, ..trait_cols])
+
+# rm
+freshwaterecol <- freshwaterecol[!pos, ]
+
+# - Proceed with creating genus col
+# everything with sp. to genus column
 freshwaterecol[
   grep("(?!.*ssp\\.)(?=[[:space:]]sp\\.)", taxon, perl = TRUE),
   `:=`(
@@ -81,36 +117,109 @@ freshwaterecol[
   )
 ]
 
-# - create species column
-setnames(freshwaterecol, "taxon", "species")
-
 # - add genera from first name of species col
 freshwaterecol[
-  !is.na(species),
+  !is.na(taxon),
   genus := sub(
     "([A-z]{1,})([[:space:]])(.+)",
-    "\\1", species
+    "\\1", taxon
   )
 ]
 
-# - clean:
+# - create species column from taxon column
+setnames(freshwaterecol, "taxon", "species")
+
+# - cleaning:
 # species col:
-# rm Lv. and spp.,
-# ?Gr., Agg., ""JDS2""
+# rm Lv. and spp., Gr., Agg.
 freshwaterecol[
   !is.na(species),
-  species := sub("Lv\\.|spp\\.", "", species)
+  species := sub("Lv\\.|spp\\.|Gr\\.|Agg\\.", "", species)
 ]
 
 # genus col:
-# rm sp., Gen, Gr.-
-# possibly duplicates introduced
-freshwaterecol[, genus := sub("([A-z]{1,})([[:space:]])(.+)", "\\1", genus)] 
-freshwaterecol[, genus := sub("([A-z]{1,})(\\-)(.+)", "\\1", genus)] 
+# rm sp., Gen, Gr.-, (new sp. GR)
+# ?possibly duplicates introduced
+freshwaterecol[, genus := sub("sp\\.|Gr\\.|Lv\\.", "", genus)]
+freshwaterecol[, genus := sub(
+  "[0-9].+|Pe 1",
+  "", genus
+)]
 
-# - handle family col
-# NAs need to be filled until next upper case word comes
+# order_or_higher column:
+# create subfamily col for NAE taxa
+# move chironomidae from order col
+# rm [Ord: ...]
+# move suborder to new column
+freshwaterecol[grep("(?i)nae", family), `:=`(
+  Subfamily_or_tribe = family,
+  family = NA_character_
+)]
+freshwaterecol[grep(".*dae", order_or_higher), `:=`(
+  family = order_or_higher,
+  order_or_higher = "Diptera"
+)]
+freshwaterecol[, order_or_higher := sub(
+  "(\\[Ord\\:)(.+)(\\])",
+  "\\2", order_or_higher
+)]
+freshwaterecol[grep("\\[UOrd\\:.+\\]", order_or_higher), `:=`(
+  suborder = order_or_higher,
+  order_or_higher = NA_character_
+)]
 
-# - handle order_or_higher column
-# check for Klass, order and so on
-# fill from one taxon to the next
+# create class & phylum column
+freshwaterecol[
+  grep("\\[Kl\\:.+\\]", order_or_higher), `:=`(
+    class = sub("(\\[Kl\\:)(.+)(\\])", "\\2", order_or_higher),
+    order_or_higher = NA_character_
+  )
+]
+freshwaterecol[
+  grep("\\[Ph\\:.+\\]", order_or_higher), `:=`(
+    phylum = sub("(\\[Ph\\:)(.+)(\\])", "\\2", order_or_higher),
+    order_or_higher = NA_character_
+  )
+]
+
+# - taxonomic corrections:
+# few order are actually class, phylum, or suborder
+freshwaterecol[
+  order_or_higher %in% c(
+    "Porifera",
+    "Nemertini",
+    "Coelenterata",
+    "Nematomorpha",
+    "Crustacea",
+    "Bryozoa"
+  ),
+  `:=`(
+    phylum = order_or_higher,
+    order_or_higher = NA_character_
+  )
+]
+freshwaterecol[
+  order_or_higher %in% c(
+    "Turbellaria",
+    "Bivalvia",
+    "Oligochaeta",
+    "Hirudinea",
+    "Polychaeta",
+    "Gastropoda"
+  ),
+  `:=`(
+    class = order_or_higher,
+    order_or_higher = NA_character_
+  )
+]
+freshwaterecol[
+  grep(
+    "Heteroptera|Hydrachnidia|Planipennia|Conchostraca", order_or_higher
+  ),
+  order_or_higher := case_when(
+    order_or_higher == "Heteroptera" ~ "Hemiptera",
+    order_or_higher == "Hydrachnidia" ~ "Trombidiformes",
+    order_or_higher == "Planipennia" ~ "Neuroptera",
+    order_or_higher == "Conchostraca" ~ "Onychura"
+  )
+]
---
+++ R/Preprocessing_raw/functions_used.R
@@ -0,0 +1,46 @@
+
+#### Copy a word in a vector until next word occurs ####
+copy_till_next_occur <- function(x) {
+  pos <- which(!is.na(x))
+
+  for (i in seq_along(pos)) {
+    end_pos <- pos[length(pos)]
+
+    if (pos[i] != end_pos) {
+      x[pos[i]:(pos[i + 1] - 1)] <- x[pos[i]]
+    }
+    if (pos[i] == end_pos) {
+      x[pos[i]:length(x)] <- x[end_pos]
+    }
+  }
+  x
+}
+
+# test <- c(
+#   NA_character_ ,
+#   "ABC",
+#   NA_character_,
+#   NA_character_,
+#   "DEF",
+#   NA_character_,
+#   "GHI",
+#   NA_character_
+# )
+
+# copy_till_next_occur(x = test)
+
+#### Capitalize first letter ####
+simpleCap <- function(x) {
+  s <- tolower(x)
+  paste0(toupper(substring(s, 1, 1)), substring(s, 2))
+}
+
+#### Google search ####
+query_google <- function(x) {
+  invisible(lapply(
+    x,
+    function(y) {
+      utils::browseURL(url = paste0("https://google.com/search?q=", y))
+    }
+  ))
+}
